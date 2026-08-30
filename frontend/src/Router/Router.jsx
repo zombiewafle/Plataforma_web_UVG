46ad733e8e4944f39784f46c5e5c35c047bb1a66
Javier Salazar <javierandre117@gmail.com>
2026-08-30 14:41:14 -0600

    Update Router.jsx

--- frontend/src/Router/Router.jsx
+++ frontend/src/Router/Router.jsx
@@ -1,23 +1,30 @@
-import Login from "../Vistas/Login"
+import Login from "../Vistas/Login";
 import RootLayout from "./RouteLayout";
 import AuthLayout from "./AuthLayout";
-import Home from '../Vistas/Home';
+import Home from "../Vistas/Home";
 import { createBrowserRouter, Navigate } from "react-router";
 
 const router = createBrowserRouter([
     {
+        path: "/",
+        element: <Navigate to="/login" replace />,
+    },
+    {
         element: <AuthLayout />,
         children: [
-            { path: "/", element: <Navigate to="/login" replace /> },
-            { path: "/login", element: <Login /> },
-
+            {
+                path: "/login",
+                element: <Login />,
+            },
         ],
     },
     {
         element: <RootLayout />,
         children: [
-            { path: "/", element: <Home /> },
-
+            {
+                path: "/home",
+                element: <Home />,
+            },
         ],
     },
 ]);
